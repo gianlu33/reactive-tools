b450618880fcf85096a61f4f33fc3908c4340ee7
Gianluca Scopelliti <gianlu.1033@gmail.com>
2020-06-23 16:03:22 +0200

    add new timing test

---
+++ examples/sgx/timing_test/sm1/src/lib.rs
@@ -0,0 +1,58 @@
+use std::net::TcpStream;
+use reactive_net::{CommandCode, CommandMessage};
+use std::time::SystemTime;
+
+const SIZE : usize = 2;
+
+//@ sm_entry
+pub fn start_test(data : &[u8]) -> ResultMessage {
+    info!("starting test");
+
+    let start_time = SystemTime::now();
+
+    if let Err(_) = ping_device() {
+        return failure(ResultCode::InternalError, None);
+    }
+
+    match SystemTime::now().duration_since(start_time) {
+            Ok(n) => info!(&format!("RTT: {} ms", n.as_millis())),
+            Err(_) => {
+                warning!("Error while checking the time difference");
+                return failure(ResultCode::InternalError, None);
+            }
+    }
+
+    success(None)
+}
+
+fn ping_device() -> Result<(), ()> {
+    let mut stream = match TcpStream::connect("127.0.0.1:6000") {
+        Ok(s) => s,
+        Err(e)  => {
+            error!(e);
+            return Err(());
+        }
+    };
+
+    info!("Connected to SM");
+
+    let payload = [0u8; SIZE];
+    let cmd = CommandMessage::new(CommandCode::Ping, Some(payload.to_vec()));
+
+    debug!(cmd);
+
+    if let Err(e) = reactive_net::write_command(&mut stream, &cmd) {
+        error!(e);
+        return Err(());
+    }
+
+    match reactive_net::read_result(&mut stream) {
+        Ok(p)   => info!("Received result"),
+        Err(e)  => {
+            error!(e);
+            return Err(());
+        }
+    };
+
+    Ok(())
+}
