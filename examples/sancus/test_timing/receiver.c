bc18dc90c517b5608a654f71decc7944fbbc6dcd
Gianluca Scopelliti <gianlu.1033@gmail.com>
2021-01-13 15:03:53 +0100

    Clean examples folder

--- examples/sancus/test_timing/receiver.c
+++
@@ -1,7 +0,0 @@
-#include <sancus/reactive.h>
-
-#include <stdio.h>
-
-SM_INPUT(receiver, data_requested, data, len) {
-  puts("[receiver] requested data");
-}
--- examples/sgx/encryption_test/sm2/src/lib.rs
+++
@@ -1,6 +0,0 @@
-//@ sm_output(send_output)
-
-//@ sm_input
-pub fn input_received(_data : &[u8]) {
-    send_output(&[]);
-}
