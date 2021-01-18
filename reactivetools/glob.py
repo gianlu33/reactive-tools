dd043db9621538619e1b6ba3de33f0cd8e6baf6c
Gianluca Scopelliti <gianlu.1033@gmail.com>
2021-01-18 17:49:50 +0100

    4 threads by default in SGX modules

--- reactivetools/glob.py
+++ reactivetools/glob.py
@@ -14,5 +14,5 @@ DEBUG_FLAG = "{}".format("--debug" if BUILD_MODE == "debug" else "")
 
 BUILD_APP = "cargo build {} {{}} --manifest-path={{}}/Cargo.toml".format(RELEASE_FLAG)
 BUILD_SGX_APP = "{} --target={}".format(BUILD_APP, SGX_TARGET)
-CONVERT_SGX = "ftxsgx-elf2sgxs {{}} --heap-size 0x20000 --stack-size 0x20000 --threads 2 {}".format(DEBUG_FLAG)
+CONVERT_SGX = "ftxsgx-elf2sgxs {{}} --heap-size 0x20000 --stack-size 0x20000 --threads 4 {}".format(DEBUG_FLAG)
 SIGN_SGX = "sgxs-sign --key {{}} {{}} {{}} {} --xfrm 7/0 --isvprodid 0 --isvsvn 0".format(DEBUG_FLAG)
